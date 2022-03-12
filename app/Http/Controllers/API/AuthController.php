9cc0a059ba9f4d3f8169d9d96d7cc80af4948585
dinushchathurya <dinushchathurya21@gmail.com>
2022-03-12 10:44:23 +0530

    feat: Create function to register user

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Hash;
+use Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'error'=>$validator->errors()
+            ], 401);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([ 
+            'user' => $user, 
+            'access_token' => $accessToken
+        ], 200);
+    }
+}
