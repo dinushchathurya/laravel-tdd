4b564e158420ef96af0f0a8db70aa58a279cd4f7
dinushchathurya <dinushchathurya21@gmail.com>
2022-03-12 11:30:09 +0530

    feat: Create api routes related to product controller

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProductController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::apiResource('/product', ProductController::class)->middleware('auth:api');
